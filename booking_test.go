623f2fb83a5b2bfd0099bcab608e13ce03ed9a75
Brandon Keene <bkeene@gmail.com>
2015-01-04 10:47:51 -0500

    more complete newDateRange() tests

--- booking_test.go
+++ booking_test.go
@@ -1,17 +1,52 @@
 package booking
 
 import (
+	"reflect"
 	"testing"
 	"time"
 )
 
 func TestDateRange(t *testing.T) {
-	t1 := time.Date(2015, 2, 1, 0, 0, 0, 0, time.UTC)
-	t2 := time.Date(2015, 2, 3, 0, 0, 0, 0, time.UTC)
-	r := newDateRange(t1, t2)
-	if r.Duration != 48*time.Hour {
-		t.Error("want", 48*time.Hour)
-		t.Error("got ", r.Duration)
+	tests := []struct {
+		t1 time.Time
+		t2 time.Time
+		r  dateRange
+	}{
+		// t1 before t2
+		{
+			time.Date(2015, 2, 1, 0, 0, 0, 0, time.UTC),
+			time.Date(2015, 2, 3, 0, 0, 0, 0, time.UTC),
+			dateRange{
+				Start:    time.Date(2015, 2, 1, 0, 0, 0, 0, time.UTC),
+				Duration: 48 * time.Hour,
+			},
+		},
+		// t2 before t1
+		{
+			time.Date(2015, 2, 3, 0, 0, 0, 0, time.UTC),
+			time.Date(2015, 2, 1, 0, 0, 0, 0, time.UTC),
+			dateRange{
+				Start:    time.Date(2015, 2, 1, 0, 0, 0, 0, time.UTC),
+				Duration: 48 * time.Hour,
+			},
+		},
+		// t1 equals t2
+		{
+			time.Date(2015, 2, 1, 0, 0, 0, 0, time.UTC),
+			time.Date(2015, 2, 1, 0, 0, 0, 0, time.UTC),
+			dateRange{
+				Start:    time.Date(2015, 2, 1, 0, 0, 0, 0, time.UTC),
+				Duration: 0,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		r := newDateRange(tt.t1, tt.t2)
+		if !reflect.DeepEqual(tt.r, r) {
+			t.Error("want", tt.r)
+			t.Error("got ", r)
+		}
 	}
 }
 
