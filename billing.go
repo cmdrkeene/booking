866310295e381c367fa9e65c2a87a398b6e0cf8e
Brandon Keene <bkeene@gmail.com>
2015-01-15 16:20:32 -0500

    implement stripeCharger

--- billing.go
+++ billing.go
@@ -2,30 +2,50 @@ package booking
 
 import (
 	"fmt"
+	"log"
 	"time"
+
+	"github.com/stripe/stripe-go"
+	"github.com/stripe/stripe-go/charge"
 )
 
 type charger interface {
 	Charge(creditCard, amount) error
 }
 
-type creditCard struct {
-	Name            string
-	Number          string
-	ExpirationMonth int
-	ExpirationYear  int
+type stripeCharger struct{}
+
+func newStripeCharger(key string) stripeCharger {
+	stripe.Key = key
+	return stripeCharger{}
 }
 
-type paymentProcessor interface {
-	Authorize(creditCard) (paymentToken, error)
-}
-
-type paymentToken struct {
-	token string
-}
-
-func (pt paymentToken) Capture(amount) error {
+func (c stripeCharger) Charge(card creditCard, amount amount) error {
+	params := &stripe.ChargeParams{
+		Amount:   uint64(amount.Cents),
+		Currency: "usd",
+		Card: &stripe.CardParams{
+			Number: card.Number,
+			Month:  card.Month,
+			Year:   card.Year,
+			CVC:    card.CVC,
+		},
+		Desc: "Apartment Reservation",
+	}
+	ch, err := charge.New(params)
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+	log.Print(ch)
 	return nil
+}
+
+type creditCard struct {
+	Number string
+	Month  string
+	Year   string
+	CVC    string
 }
 
 const minAmountCents = 100    // $1.00
@@ -43,11 +63,6 @@ type entry struct {
 	Amount amount
 	Date   time.Time
 }
-
-type ledger map[guestId][]entry
-
-func (l ledger) Credit(guestId, amount) error { return nil }
-func (l ledger) Debit(guestId, amount) error  { return nil }
 
 type rateCode int
 
