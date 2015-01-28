000cfb86369e7e45dcfb8c2c7c18966b6b9b0e7b
Brandon Keene <bkeene@gmail.com>
2015-01-28 12:04:46 -0500

    run tests on on autotest start

--- autotest.sh
+++ autotest.sh
@@ -4,10 +4,9 @@ which entr > /dev/null
 if [ $? -ne 0 ]; then
     echo "installing entr"
     brew install entr
-else
-	echo "watching go files for testing"
 fi
 
+go test ./...
 while sleep 1; do
   ls *.go | entr -d -c go test ./...
 done
